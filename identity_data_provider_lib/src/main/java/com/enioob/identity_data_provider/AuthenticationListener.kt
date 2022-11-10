2062fd23900846ee2396b1251639fe58bf47c6f3
Enio Biondić <37383564+Blondhino@users.noreply.github.com>
2022-11-10 11:25:30 +0100

    prevent unnecessary overrides for AuthenticationListener

--- identity_data_provider_lib/src/main/java/com/enioob/identity_data_provider/AuthenticationListener.kt
+++ identity_data_provider_lib/src/main/java/com/enioob/identity_data_provider/AuthenticationListener.kt
@@ -3,17 +3,17 @@ package com.enioob.identity_data_provider
 import com.enioob.identity_data_provider.model.IdpUser
 
 interface AuthenticationListener {
-  fun onLoadingStatusChanged(isLoading : Boolean)
-  fun onLogIn()
-  fun onRegister(user : IdpUser)
-  fun onLogOut()
-  fun onPasswordReset()
-  fun onVerificationEmailResent()
-  fun onEmailVerified(user : IdpUser)
-  fun onForgotPasswordMailSent()
-  fun onError(message : String)
-  fun onUserUpdated(user : IdpUser)
-  fun onTokensRefreshed()
-  fun onForgottenPasswordReset()
-  fun onUserDeleted()
+  fun onLoadingStatusChanged(isLoading : Boolean) = {}
+  fun onLogIn() = {}
+  fun onRegister(user : IdpUser) = {}
+  fun onLogOut() = {}
+  fun onPasswordReset() = {}
+  fun onVerificationEmailResent() = {}
+  fun onEmailVerified(user : IdpUser) = {}
+  fun onForgotPasswordMailSent() = {}
+  fun onError(message : String) = {}
+  fun onUserUpdated(user : IdpUser) = {}
+  fun onTokensRefreshed() = {}
+  fun onForgottenPasswordReset() = {}
+  fun onUserDeleted() = {}
 }
